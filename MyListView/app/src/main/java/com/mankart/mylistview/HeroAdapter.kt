5c7688cbfdc2c566d7d2bd0698b371d029d89c08
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-08-10 20:54:51 +0700

    create project MyListView and implementing adapter

---
+++ MyListView/app/src/main/java/com/mankart/mylistview/HeroAdapter.kt
@@ -0,0 +1,47 @@
+package com.mankart.mylistview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
+    internal var heroes = arrayListOf<Hero>()
+
+    override fun getCount(): Int = heroes.size
+
+    override fun getItem(position: Int): Any = heroes[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var itemView = convertView
+        if (itemView == null) {
+            itemView = LayoutInflater.from(context).inflate(R.layout.item_hero, parent, false)
+        }
+
+        var viewHolder = ViewHolder(itemView as View)
+
+        val  hero = getItem(position) as Hero
+        viewHolder.bind(hero)
+
+        return itemView
+    }
+
+    private inner class ViewHolder internal constructor(view: View) {
+
+        private val txtName: TextView = view.findViewById(R.id.txt_name)
+        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
+        private val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
+
+        internal fun bind(hero: Hero) {
+            txtName.text = hero.name
+            txtDescription.text = hero.description
+            imgPhoto.setImageResource(hero.photo)
+        }
+
+    }
+}
