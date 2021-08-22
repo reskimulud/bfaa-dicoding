8c2a6e76a2d6a44509ae66da19aee2f5e0049d7c
Reski Mulud Muchamad <reski.mulud@gmail.com>
2021-08-22 23:48:37 +0700

    create project MyNavigation

---
+++ MyCustomView/app/src/main/java/com/mankart/mycustomview/MyButton.kt
@@ -0,0 +1,52 @@
+package com.mankart.mycustomview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+
+class MyButton : AppCompatButton {
+
+    private var enabledBackground: Drawable? = null
+    private var disabledBackground: Drawable? = null
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attr: AttributeSet) : super(context, attr) {
+        init()
+    }
+
+    constructor(context: Context, attr: AttributeSet, defStyleAttr: Int) : super(context, attr, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        background = when {
+            isEnabled -> enabledBackground
+            else -> disabledBackground
+        }
+
+        setTextColor(txtColor)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = when {
+            isEnabled -> "Submit"
+            else -> "Isi dulu!"
+        }
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ResourcesCompat.getDrawable(resources, R.drawable.bg_button, null)
+        disabledBackground = ResourcesCompat.getDrawable(resources, R.drawable.bg_button_disabled, null)
+    }
+
+}
---
+++ MyNavigation/app/src/main/java/com/mankart/mynavigation/DetailCategoryFragment.kt
@@ -0,0 +1,49 @@
+package com.mankart.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.mankart.mynavigation.databinding.FragmentDetailCategoryBinding
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+//        val dataDescription = arguments?.getInt(CategoryFragment.EXTRA_STOCK)
+
+        val bundle = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle)
+        val dataName = bundle.name
+        val dataDescription = bundle.stock
+
+        binding.tvCategoryName.text = dataName
+        binding.tvDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
