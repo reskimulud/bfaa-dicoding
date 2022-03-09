e888febf6c99eb9d01389cb5cc7226cb7136dc66
Reski Mulud Muchamad <63949402+reskimulud@users.noreply.github.com>
2022-03-09 11:22:11 +0700

    crate project MyDataStore with implementing dark mode toggle and save the state to data store

---
+++ MyDataStore/app/src/main/java/com/mankart/mydatastore/SettingPreferences.kt
@@ -0,0 +1,37 @@
+package com.mankart.mydatastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class SettingPreferences private constructor(val dataStore: DataStore<Preferences>) {
+    private val THEME_KEY = booleanPreferencesKey("theme_setting")
+
+    fun getThemeSetting() : Flow<Boolean> {
+        return dataStore.data.map {
+            it[THEME_KEY] ?: false
+        }
+    }
+
+    suspend fun saveThemeSetting(isDarkModeActive: Boolean) {
+        dataStore.edit {
+            it[THEME_KEY] = isDarkModeActive
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SettingPreferences? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>) : SettingPreferences {
+            return INSTANCE ?: synchronized(this) {
+                val instance = SettingPreferences(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
